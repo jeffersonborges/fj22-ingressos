b1a4e6fedbb63811cd26743d13c306eb7ee476cd
jeffersonborges <jefferson.gmb@gmail.com>
2020-06-17 23:06:01 -0300

    Inclusão dos arquivos SessaoDao e SessaoForm

---
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -0,0 +1,25 @@
+package br.com.caelum.ingresso.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
+public class SessaoDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void save(Sessao sessao) {
+		manager.persist(sessao);
+	}
+
+	// "SQL" na verdade é HQL
+	public List<Sessao> buscaSessoesDaSala(Sala sala) {
+		return manager.createQuery("select s from Sessao s where s.sala = :sala", Sessao.class)
+				.setParameter("sala", sala).getResultList();
+	}
+}
